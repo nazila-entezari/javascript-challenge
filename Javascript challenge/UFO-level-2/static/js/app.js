c2bc3ae5b2083284f4fc45a23ca36aedab7e0900
nazila-entezari <65842917+nazila-entezari@users.noreply.github.com>
2020-09-19 16:34:55 -0400

    added the changes

--- Javascript challenge/UFO-level-2/static/js/app.js
+++ Javascript challenge/UFO-level-2/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+function TableAutoPopulate(data) {
+    d3.select("tbody").html("");
+    data.forEach((selection) => {
+        var tableRow = d3.select("tbody").append("tr");
+        Object.values(selection).forEach((value) => {
+            var tableData = tableRow.append("td");
+            tableData.text(value);
+        });
+    })
+}
+
+TableAutoPopulate(tableData);
+
+
+function clicktrigger() {
+    d3.event.preventDefault();
+    var date = d3.select("#datetime").property("value");
+    var filteredDateTime = tableData;
+    if (date) {
+        filteredDateTime = filteredDateTime.filter((row) => row.datetime === date);
+    }
+    TableAutoPopulate(filteredDateTime);
+}
+
+d3.selectAll("#filter-btn").on("click", clicktrigger);
